2c37078217439d2f4c4dd6728fa7502d50117919
Florent Espanet <florent@lumapps.com>
2015-12-11 13:12:29 +0100

    Add upload button on frontend.

--- project/static/js/app.js
+++ project/static/js/app.js
@@ -1,10 +1,21 @@
-var app = angular.module("app", ["ngCookies"]);
+var app = angular.module("app", ["ngFileUpload"]);
 
-app.controller("BodyCtrl", function ($scope, User, $cookies) {
+app.controller("BodyCtrl", function ($scope, Upload) {
 
-    function main() {
-        console.log("In the main function.")
+    $scope.uploading = false;
+    $scope.progress = 0;
+
+    $scope.upload = function(file) {
+        Upload.upload({
+            url: '/upload/',
+            data: {file: file}
+        }).then(function (response) {
+            $scope.uploading = false;
+        }, function (response) {
+            $scope.uploading = false;
+        }, function (evt) {
+            $scope.uploading = true;
+            $scope.progress = parseInt(100.0 * evt.loaded / evt.total);
+        });
     };
-
-    main();
 });
